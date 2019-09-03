2563c47a92d4467a20fab92fbfdd4ef63ddef27b
leilei <18958740095@163.com>
2019-09-03 11:27:14 +0800

    初始化百思不得姐的app

---
+++ BuDeJie/BuDeJie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuDeJie
+//
+//  Created by mushroom on 2019/9/3.
+//  Copyright © 2019 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BuDeJie/BuDeJie/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BuDeJie
+//
+//  Created by mushroom on 2019/9/3.
+//  Copyright © 2019 xiaomage. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
